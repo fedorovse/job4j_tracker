89dd9f25c1980bcf3ea1093bd21a165428737955
fedorovse <ingor-ru@mail.ru>
2022-06-14 18:31:25 +0700

    2. Аттестация [#49777 #126011]_

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -76,7 +76,6 @@ public class Analyze {
     public static Tuple bestSubject(Stream<Pupil> stream) {
         return stream.flatMap(p -> p.getSubjects().stream())
                 .collect(Collectors.groupingBy(Subject::getName,
-                        LinkedHashMap::new,
                         Collectors.summingDouble(Subject::getScore)))
                 .entrySet().stream()
                 .map(m -> new Tuple(m.getKey(), m.getValue()))
